a8aa8a75e4cf7a440d26cb365ced95b2bd108fe6
sourabh1o1 <sourabh@cogcons.com>
2018-07-20 11:10:07 +0530

    longest common subseuence
    Signed-off-by: sourabh1o1 <sourabh@cogcons.com>

---
+++ dynamicprogramming/longestCommonSubsequence.py
@@ -0,0 +1,19 @@
+def longest_subsequence(str1, str2):
+    n = len(str1) + 1
+    m = len(str2) + 1
+    matrix = [[0 for x in range(m)] for y in range(n)]
+
+    for i in range(1, n):
+        for j in range (1, m):
+            if str1[i - 1] == str2[j - 1]:
+                matrix[i][j] = matrix[i - 1][j - 1] + 1
+            else:
+                matrix[i][j] = max(matrix[i][j - 1], matrix[i - 1][j - 1])
+
+    return matrix[n - 1][m - 1]
+
+
+if __name__ == '__main__':
+    str1 = input()
+    str2 = input()
+    print(longest_subsequence(str1, str2))
